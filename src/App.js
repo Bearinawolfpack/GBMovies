435bcf7e90a581c634942c7d907d38c811aefc47
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-22 13:01:02 -0400

    Components are staged. Routing is next.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Header from './Components/AppComponents/Header';
+import MoviesDisplay from './Components/MovieComponents/MoviesDisplay';
 
 const App = () => (
 	<>
 		<Header />
 		<h1>Hello World</h1>
+		<MoviesDisplay />
 	</>
 );
 
